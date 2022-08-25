c3a17c147f4da0501fdb312f4a50dc099a188b64
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-08-25 15:55:35 +0900

    [feat] cors 설정 추가

--- Server/src/main/java/com/team23/PreProject/Controller/MainController.java
+++ Server/src/main/java/com/team23/PreProject/Controller/MainController.java
@@ -24,73 +24,78 @@ public class MainController {
     @Autowired
     public ProfileService profileService;
 
-
+    @CrossOrigin
     @PostMapping(value = "/api/answer/{uid}")
     public ResponseEntity answer_create(@PathVariable String uid) {
-        System.out.println("answer_create " + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" answer_create " + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(answerService.create(uid),HttpStatus.OK);
     }
+    @CrossOrigin
     @PostMapping(value = "/api/answer/check/{uid}")
     public ResponseEntity answer_checking(@PathVariable String uid) {
-        System.out.println("answer_checking" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" answer_checking" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(answerService.checked(uid),HttpStatus.OK);
     }
 
-
+    @CrossOrigin
     @GetMapping(value = "/api/answer/{uid}")
     public ResponseEntity answer_read(@PathVariable String uid) {
-        System.out.println("answer_read" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" answer_read" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(answerService.read(uid),HttpStatus.OK);
     }
 
-
+    @CrossOrigin
     @PutMapping(value = "/api/answer/{uid}")
     public ResponseEntity answer_update(@PathVariable String uid) {
-        System.out.println("answer_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" answer_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(answerService.update(uid),HttpStatus.OK);
     }
+
+    @CrossOrigin
     @DeleteMapping(value = "/api/answer/{uid}")
     public ResponseEntity answer_delete(@PathVariable String uid) {
-        System.out.println("answer_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" answer_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(answerService.delete(uid),HttpStatus.OK);
     }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @CrossOrigin
     @PostMapping(value = "/api/comment/{uid}")
     public ResponseEntity comment_create(@PathVariable String uid) {
-        System.out.println("comment_create" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" comment_create" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(commentService.create(uid),HttpStatus.OK);
     }
 
-
+    @CrossOrigin
     @GetMapping(value = "/api/comment/{uid}")
     public ResponseEntity comment_read(@PathVariable String uid) {
-        System.out.println("comment_read" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" comment_read" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(commentService.read(uid),HttpStatus.OK);
     }
-
+    @CrossOrigin
     @PutMapping(value = "/api/comment/{uid}")
     public ResponseEntity comment_update(@PathVariable String uid) {
-        System.out.println("comment_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" comment_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(commentService.update(uid),HttpStatus.OK);
     }
-
+    @CrossOrigin
     @DeleteMapping(value = "/api/comment/{uid}")
     public ResponseEntity comment_delete(@PathVariable String uid) {
-        System.out.println("comment_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" comment_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(commentService.delete(uid),HttpStatus.OK);
     }
 
 
-
+    @CrossOrigin
     @PostMapping(value = "/api/users/picture/{uid}")
     public ResponseEntity profile_pic_update(@PathVariable String uid) {
-        System.out.println("profile_pic_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" profile_pic_update" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(profileService.update(uid),HttpStatus.OK);
     }
 
+    @CrossOrigin
     @DeleteMapping(value = "/api/users/picture/{uid}")
     public ResponseEntity profile_pic_delete(@PathVariable String uid) {
-        System.out.println("profile_pic_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
+        System.out.println("uid = "+uid+" profile_pic_delete" + LocalDate.now() +" "+ LocalTime.now().format(DateTimeFormatter.ofPattern("HH시 mm분 ss초")));
         return new ResponseEntity(profileService.delete(uid),HttpStatus.OK);
     }
 
