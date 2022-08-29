aec9e4e433c7918d95b2b129a30f598315941131
Moondoyeon <mmmdo21@gmail.com>
2022-08-29 13:22:11 +0900

    Design: header 컴포넌트

--- Client/src/components/MyHeader.js
+++ Client/src/components/MyHeader.js
@@ -3,74 +3,126 @@ import styled from 'styled-components';
 import { faStackOverflow } from '@fortawesome/fontawesome-free-brands';
 import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import MyButton from './MyButton';
-// import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+/* eslint-disable react/prop-types */
 axios.defaults.withCredentials = false;
 
-const MyHeader = () => {
+const MyHeader = ({ viewModal, setModal }) => {
   const navigate = useNavigate();
-
-  // const [data, setData] = useState('');
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://115.40.230.109:8080/api/users/1`)
-  //     .then((res) => {
-  //       setData(res.data);
-  //       console.log(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
+  const [isLogin, setIsLogin] = useState(true);
+  const viewLogout = () => {
+    setModal(!viewModal);
+  };
   return (
     <Container>
-      <FontAwesomeIcon icon={faBars} className="menubar" />
-      <Logo>
-        <FontAwesomeIcon icon={faStackOverflow} />
-        Stack<b>Overflow</b>
-      </Logo>
-      <div className="hardcoding">
-        <span>About</span>
-        <span>Products</span>
-        <span>For Teams</span>
-      </div>
-      <form>
-        <FontAwesomeIcon icon={faMagnifyingGlass} className="magnifying" />
-        <input type="text" placeholder="Search..." />
-      </form>
-      <BtnWapper>
-        <MyButton
-          text={`Log in`}
-          type={'skyblue'}
+      <FontAwesomeIcon
+        icon={faBars}
+        className="menubar"
+        onClick={() => {
+          setIsLogin(!isLogin);
+        }}
+      />
+      <Wrapper>
+        {/* <FontAwesomeIcon
+          icon={faBars}
+          className={isLogin ? 'displayNone' : 'menubar'}
+        /> */}
+        <Logo
           onClick={() => {
-            navigate('/login');
+            navigate('/');
           }}
+        >
+          <FontAwesomeIcon
+            icon={faStackOverflow}
+            className={isLogin ? 'loginLogo' : ''}
+          />
+          Stack<b>Overflow</b>
+        </Logo>
+        <div className={isLogin ? 'loginHardcoding' : 'hardcoding'}>
+          <span className={isLogin ? 'displayNone' : ''}>About</span>
+          <span>Products</span>
+          <span className={isLogin ? 'displayNone' : ''}>For Teams</span>
+        </div>
+        <form className={isLogin ? 'loginSearch' : ''}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="magnifying" />
+          <input type="text" placeholder="Search..." />
+        </form>
+        <BtnWapper>
+          {isLogin ? (
+            <>
+              <Link
+                to="/mypage"
+                style={{ textDecoration: 'none' }}
+                className="loginMypage"
+              >
+                <span>Mypage</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <MyButton
+                text={`Log in`}
+                type={'skyblue'}
+                onClick={() => {
+                  navigate('/login');
+                }}
+              />
+              <MyButton
+                text={`Sign up`}
+                type={'blue'}
+                onClick={() => {
+                  navigate('/signup');
+                }}
+              />
+            </>
+          )}
+        </BtnWapper>
+        <FontAwesomeIcon
+          icon={faBars}
+          className={isLogin ? 'menubar loginMenubar' : 'displayNone'}
+          onClick={viewLogout}
         />
-        <MyButton
-          text={`Sign up`}
-          type={'blue'}
-          onClick={() => {
-            navigate('/signup');
-          }}
-        />
-      </BtnWapper>
-      {/* <h1>{data.writerId}</h1>
-      <h1>{data.postContent}</h1> */}
+      </Wrapper>
     </Container>
   );
 };
-
 const Container = styled.header`
   background-color: #f8f9f9;
   border-top: solid 3px #f2740d;
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: center;
   padding: 10px;
+  .displayNone {
+    display: none;
+  }
+  .loginMenubar {
+    width: 3%;
+  }
+  .loginMypage {
+    width: 3%;
+  }
+  .loginHardcoding {
+    width: 3%;
+  }
+  .loginLogo {
+    width: 10%;
+  }
+  .loginSearch {
+    width: 70%;
+  }
+`;
+const Wrapper = styled.div`
+  width: 1330px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 30px;
   .menubar {
-    font-size: 20px;
+    width: 2%;
+    font-size: 16px;
     margin-left: 10px;
     cursor: pointer;
   }
@@ -78,37 +130,48 @@ const Container = styled.header`
     display: flex;
     white-space: nowrap;
     padding: 10px;
+    width: 18%;
+    justify-content: space-around;
   }
   span {
-    margin: 0 20px;
-    font-size: 18px;
+    font-size: 14px;
     color: #363b3f;
   }
+  form {
+    width: 55%;
+    display: flex;
+  }
   input {
-    width: 550px;
-    height: 42px;
-    font-size: 18px;
+    flex-basis: 100%;
+    height: 30px;
+    font-size: 14px;
     color: #363b3f;
     text-indent: 35px;
+    border: 1px solid #8a939b;
   }
   input:focus {
     border: 1px solid cornflowerblue;
     border-radius: 2px;
     outline: none;
-    box-shadow: 0 0 0 5px #cde9fe;
+    box-shadow: 0 0 0 3px #cde9fe;
   }
   .magnifying {
     position: relative;
     left: 30px;
-    color: gray;
-    font-size: 20px;
+    top: 6px;
+    color: #8a939b;
+    font-size: 16px;
+  }
+  a {
+    text-decoration: none;
   }
 `;
-
 const Logo = styled.div`
+  width: 10%;
   color: black;
   display: flex;
-  font-size: 25px;
+  justify-content: center;
+  font-size: 18px;
   cursor: pointer;
   margin: 0 10px;
   b {
@@ -116,10 +179,13 @@ const Logo = styled.div`
     padding-left: 5px;
   }
 `;
-
 const BtnWapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  width: 150px;
+  button:first-of-type {
+    margin: 0px 10px;
+  }
+  button:nth-of-type(2) {
+    width: 80px;
+  }
 `;
 export default MyHeader;
