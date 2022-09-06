d1b623a014d72a1d9464ed9c5ba395de710d79b6
wonyong92 <80381715+wonyong92@users.noreply.github.com>
2022-09-06 12:51:26 +0900

    BackEnd v0.1 (#46)
    * [Feat] MainTitle method add
    
    * [feat] api 테스트용 기능 구현 + restdocs 생성
    
    * [feat] cors 설정 추가
    
    * [feat] 프로젝트 구조 + DB 스키마 적용
    
    * 오타 제거
    
    * [feat] controller 분리
    
    * [feat] controller 어노테이션 빠진것 수정
    
    * [feat] post 방식 요청 추가
    
    * [feat] 페이지네이션+sort+hibernate 네이밍 문제 해결
    
    * 엔티티 맵핑 중
    
    * [feat] 질문글 작성 기능 + 유저 연결
    
    * [feat] 회원가입 메소드 간단 구현
    
    * 오타 수정
    
    * ascii docs 비활성화
    
    * [feat] 회원 정보 확인 - dto 미적용으로 패스워드까지 리턴 중
    
    * [feat] 특정 유저의 작성글 조회
    
    * [feat] post 수정 기능 추가
    
    * [feat] delete 기능 추가
    
    * [feat] 질문글 간단 조회
    
    * [feat] 멤버 생성시 프로필 자동 생성, 프로필 조회 기능
    
    * [feat] 비밀번호 변경 기능 추가
    
    * [feat] deleted 유저를 우선 생성하도록 기능 추가
    
    * [feat] member 삭제 기능 구현, deleted 유저 자동 삽입, memer_post 업데이트를 통해 post 연결 이동, post는 데이터 유지
    
    * [feat] deleted 유저에 대한 access 방지
    
    * [feat] ZonedDateTime 적용
    
    * [feat ] origin 설정
    
    * [feat] 시작 데이터 생성
    
    * [feat] answer create 적용 - 근범
    
    * [feat] answers 메소드 이식, DDL 수정
    
    * [feat] post_vote 기능 추가, delete member 문제 발생 수정
    
    * [feat] view_count 증가 업데이트
    
    * [feat] 기능 오류 수정
    
    * ㅇㅇㅇ
    
    * [feat] jwt 로그인 구현 + post 삭제, 비밀번호 업데이트 에러 발생
    
    * ㅇㅇㅇㅇ
    
    * [feat] delete post 수정 - 정상작동, 더미데이터 삽입
    
    * [feat] comment 기능 병합
    
    * [feat] answer 기능 구현
    
    * [feat] 로그인시 응답 내용 추가, 토큰 로그인 구현, answers By member 기능 추가
    
    * total page 계산 수정
    
    * [feat] 토큰 로그인/로그아웃/리프레시 적용
    
    * [feat] 로그인/로그아웃/로그인상태 확인 기능 수정
    
    * [feat] comment, answer_vote post_vote 적용
    
    * [feat] 태그 기능 구현 + 질문글 생성, 업데이트, 삭제 + 질문글 조회 응답 변경
    
    * [feat] findTag 기능 수정
    
    * [feat] 기능수정 , 태그 기능 추가
    
    * [feat] 태그 기능 수정,  프로필 이미지 업로드, 다운로드 기능 추가
    
    * git ignore 추가
    
    * Update .gitignore
    
    * [patch] 응답 dto 정리, 답변 생성,삭제시 프로필의 answers 반영 , 답변 채택 기능 추가 ,답변 채택 정보에 대해 포스트 정보 업데이트 기능 추가
    
    * [feat] 검색어 사전 기능 구현, 검색어 간단 학습 기능 구현, 질문글 제목 검색 기능 구현
    
    * [feat] 권한 설정 + jwt 인증 에러 핸들링 + 필요한 요청에 사용자 무결성 확인 기능 추가
    
    * [feat] 코맨트에 프로필이미지 정보 삽입, 프로필 이미지 png로 고정,  유저 생성시 기본 프로필 이미지 설정,  기타 응답 수정
    
    * Create default.yml

---
+++ Server/src/main/java/com/team23/PreProject/member/dto/tokenLogin.java
@@ -0,0 +1,13 @@
+package com.team23.PreProject.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class tokenLogin {
+    String msg;
+    String userid;
+}
